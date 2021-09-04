ceef6ec0e1fa8c3e610cb8b474e27297b56ca2ca
Mike McAllister <mike@logikbox.com>
2021-09-04 17:12:58 -0400

    data in for card 1

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/button-has-type */
 import React, { FunctionComponent, useState } from 'react';
 import classnames from 'classnames';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import BoxScore from '../boxScore/BoxScore';
 import TeamLogo from '../teamLogo/TeamLogo';
 import { useMergeState } from '../../hooks';
 
@@ -13,28 +16,49 @@ type BannerProps = {
 };
 
 const Banner: FunctionComponent<BannerProps> = ({ className, children }) => {
-  console.log('here', data.league.standings[0][0].team.logo);
-  // const standings = data.league.standings[0];
-
-  // let state = useState({});
-  // state = {
-  //   standings,
-  //   visibleRank: 1,
-  //   rankLogo: standings[0].team.logo,
-  // };
+  const league = data.league.standings[0];
+  console.log('here', league);
 
   const initialState = {
-    standings: data.league.standings[0],
     visibleRank: 1,
-    rankLogo: data.league.standings[0][0].team.logo,
+    rankLogo: league[0].team.logo,
+    team: league[0].team.name,
+    boxScore: league[0].all,
   };
 
   const [state, setState] = useMergeState(initialState);
-  const { standings, visibleRank, rankLogo } = state;
+  const { standings, visibleRank, rankLogo, team } = state;
 
   const initialValues = {};
 
-  console.log(state.rankLogo);
+  const wrapRank = (rank: number, direction: number): number => {
+    let tempRank = rank;
+    tempRank += direction;
+    if (tempRank === 20) tempRank = 0;
+    if (tempRank === -1) tempRank = 19;
+    return tempRank;
+  };
+
+  const upClickHandler = (): any => {
+    setState({
+      visibleRank: wrapRank(state.visibleRank, 1),
+      rankLogo: league[visibleRank].team.logo,
+      team: league[visibleRank].team.name,
+      boxScore: league[visibleRank].all,
+    });
+  };
+
+  const downClickHandler = (): any => {
+    setState({
+      visibleRank: wrapRank(state.visibleRank, -1),
+      rankLogo: league[visibleRank].team.logo,
+      team: league[visibleRank].team.name,
+      boxScore: league[visibleRank].all,
+      goalDiff: league[visibleRank].goalsDiff,
+      points: league[visibleRank].points,
+      form: league[visibleRank].form,
+    });
+  };
 
   return (
     <div className={classnames('container-fluid', className)}>
@@ -42,7 +66,49 @@ const Banner: FunctionComponent<BannerProps> = ({ className, children }) => {
         <div className="col-sm">
           <Card>
             <div className="card-body">
-              <TeamLogo logo={state.rankLogo} team="noone" size="large" />
+              <div className="arrow_container">
+                <div className="arrows">
+                  <Button onClick={upClickHandler}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-up"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+                      />
+                    </svg>
+                  </Button>
+                  <Button onClick={downClickHandler}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-down"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+                      />
+                    </svg>
+                  </Button>
+                </div>
+              </div>
+              <TeamLogo logo={state.rankLogo} team={team} size="lrg" />
+              <BoxScore
+                team={state.team}
+                boxScore={state.boxScore}
+                goalDiff={state.goalDiff}
+                points={state.points}
+                rank={state.visibleRank}
+                form={state.form}
+              />
             </div>
           </Card>
         </div>
---
+++ src/components/boxScore/BoxScore.tsx
@@ -0,0 +1,78 @@
+import React, { FunctionComponent, useState } from 'react';
+import classnames from 'classnames';
+
+import './BoxScore.scss';
+
+type BoxScoreProps = {
+  team: string;
+  boxScore: any;
+  rank: number;
+  goalDiff: number;
+  points: number;
+  form: string;
+};
+
+const BoxScore: FunctionComponent<BoxScoreProps> = ({
+  team,
+  boxScore,
+  rank,
+  goalDiff,
+  points,
+  form,
+}) => {
+  return (
+    <div>
+      <div className="football_title">{team}</div>
+      <br />
+      <table className="footbal__table">
+        <tbody>
+          <tr className="footbal__table_tr">
+            <th className="football__table__header">
+              <div aria-label="Rank">Rank</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label="Matches played">MP</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label="Wins">W</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label=" Draws">D</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label=" Losses">L</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label=" Goals scored">GF</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label=" Goals against">GA</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label="Goal difference">GD</div>
+            </th>
+            <th className="football__table__header">
+              <div aria-label="Points">Pts</div>
+            </th>
+          </tr>
+          <tr>
+            <td className="football__table__td__div">{rank}</td>
+            <td className="football__table__td__div">{boxScore.played}</td>
+            <td className="football__table__td__div">{boxScore.win}</td>
+            <td className="football__table__td__div">{boxScore.draw}</td>
+            <td className="football__table__td__div">{boxScore.lose}</td>
+            <td className="football__table__td__div">{boxScore.goals.for}</td>
+            <td className="football__table__td__div">{boxScore.goals.against}</td>
+            <td className="football__table__td__div">{goalDiff}</td>
+            <td className="football__table__td__div">{points}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="football_rank">
+        <span className="bold">Form:</span> {form}
+      </div>
+    </div>
+  );
+};
+
+export default BoxScore;
